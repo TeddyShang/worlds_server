fc41027a7e18277377f7dbaedf909f79d9dc9faa
TeddyShang <tshang3@gatech.edu>
2020-01-28 13:14:04 -0500

    Profile and MediaMetaData classes with all requisite fields

---
+++ server/src/main/java/worlds/server/MediaMetaData.java
@@ -0,0 +1,26 @@
+package worlds.server;
+
+import lombok.Data;
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+
+@Data
+class MediaMetaData {
+    @Id private String id;
+
+    private String creatorId;
+    private Long dateUploaded;
+    private String roomInformation;
+    private String urlToMedia;
+
+    MediaMetaData(){}
+
+    MediaMetaData(String creatorId, String roomInformation, String urlToMedia) {
+        this.creatorId = creatorId;
+        this.roomInformation = roomInformation;
+        this.urlToMedia = urlToMedia;
+        Date date = new Date();
+        this.dateUploaded = date.getTime();
+    }
+}
