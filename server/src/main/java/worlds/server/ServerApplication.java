f158e691ef69c8020db48cf1d2b4e77b265d6347
TeddyShang <tshang3@gatech.edu>
2020-02-11 12:39:54 -0500

    IVRE-171 and IVRE-164 support allowed CORS mapping for /bookings

--- server/src/main/java/worlds/server/ServerApplication.java
+++ server/src/main/java/worlds/server/ServerApplication.java
@@ -1,6 +1,10 @@
 package worlds.server;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -8,4 +12,13 @@ public class ServerApplication {
   public static void main(String... args) {
     SpringApplication.run(ServerApplication.class, args);
   }
+  @Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/bookings").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
