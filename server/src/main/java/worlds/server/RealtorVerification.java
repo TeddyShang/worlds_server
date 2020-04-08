2e24614d76e23ccb5ac7753e22b8a7ffe111158e
Shang, Ted Yinchu <ted.shang@gatech.edu>
2020-04-07 21:36:11 -0400

    Code Review

--- server/src/main/java/worlds/server/RealtorVerification.java
+++ server/src/main/java/worlds/server/RealtorVerification.java
@@ -8,6 +8,11 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class RealtorVerification{
+    /**
+     * RequestVerificationToken, and EntityType were both part of form data when inspecting the code on the website
+     * RequestVerificationToken likely for CSRF protection
+     * EntityType likely refers to the user type for the serach
+     */
     String RequestVerificationToken = "wA5ke9KMy0ZJxugGYZo1d2qj50W3Go0GiEWVKaqDPlMPsoqps66As1ZdnSAhMQs4R4iCRj1F_tMgVVrNOb4z9i9ER2Of4B0EZzQaZqH2mMc1";
     String EntityType = "RE-PR";
     String submit = "Search";
@@ -16,6 +21,14 @@ public class RealtorVerification{
 
     RealtorVerification(){}
 
+    /**
+     * POSTs and parses HTML response
+     * @param firstName
+     * @param lastName
+     * @param realtorId
+     * @return Boolean wheter or not the realtor's information has been verified
+     * @throws IOException
+     */
     public Boolean verifyUser(String firstName, String lastName, String realtorId) throws IOException {
         if (firstName == null || lastName == null || realtorId == null) {
             return false;
@@ -28,12 +41,15 @@ public class RealtorVerification{
         .data("AuthorizationNumber", realtorId)
         .data("submit", submit)
         .post();
+
+        //only the results table has this class styling for now 03/2020
         Elements tableRows = document.getElementsByClass("bg-light text-center");
 
         //check to see if no results
         if(tableRows.isEmpty()) {
             return false;
         }
+
         //should not have more than one result given unique realtor Id
         Element desiredRow = tableRows.first();
 
