755c629a0b48f17774fbfd6399ff49180958c32c
Shang, Ted Yinchu <ted.shang@gatech.edu>
2020-04-09 19:05:48 -0400

    Code Review

--- server/src/main/java/worlds/server/BookingStatus.java
+++ server/src/main/java/worlds/server/BookingStatus.java
@@ -1,5 +1,12 @@
 package worlds.server;
 
+/**
+ * PENDING: Realtor has submitted booking and paid
+ * MATCHED: Realtor's pending booking matched with appropriate realtor
+ * COMPLETED: Media uploaded and confirmed by realtor and content creator
+ * CANCELLED: Pending booking cancelled by realtor
+ * TENTATIVE: Content creator has uploaded all media and marked the booking as done
+ */
 enum BookingStatus {
     PENDING, MATCHED, COMPLETED, CANCELLED, TENTATIVE
 }
