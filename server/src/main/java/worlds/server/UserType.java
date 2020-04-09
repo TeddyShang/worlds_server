2970bf7dd7bb138048f498590ff66325b72bfbc7
Freda Chen <fredachen98@gmail.com>
2020-04-09 18:16:42 -0400

    UserType.java reviewed

--- server/src/main/java/worlds/server/UserType.java
+++ server/src/main/java/worlds/server/UserType.java
@@ -1,5 +1,10 @@
 package worlds.server;
 
+/**
+ * STAFF: User that is an Admin
+ * REALTOR: User that is Realtor
+ * CREATOR: User that is a Creator
+ */
 enum UserType {
     STAFF, REALTOR, CREATOR
 }
