5c049780ac14c1e987bb09f192f18105c1fc6d14
Freda Chen <fredachen98@gmail.com>
2020-04-09 16:47:20 -0400

    UserRepository.java javadoc

--- server/src/main/java/worlds/server/UserRepository.java
+++ server/src/main/java/worlds/server/UserRepository.java
@@ -3,6 +3,10 @@ package worlds.server;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, String> {
+    /**
+     * @param email An email to find a user with
+     * @return User user associated with that email
+     */
     User findByEmail(String email);
 
 }
