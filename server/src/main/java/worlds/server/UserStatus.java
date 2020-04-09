51c995edd2436c5c5b0f205f9695c9d5b3bc8b93
Freda Chen <fredachen98@gmail.com>
2020-04-09 18:08:58 -0400

    UserStatus reviewed

--- server/src/main/java/worlds/server/UserStatus.java
+++ server/src/main/java/worlds/server/UserStatus.java
@@ -1,5 +1,12 @@
 package worlds.server;
 
+/**
+ * PENDING: A user has not yet been reviewed by an Admin. This user cannot access the 
+ *          platform until they have been marked as “APPROVED”
+ * APPROVED: A user that has been reviewed by an Admin. This user can access the platform.
+ * BANNED: A user that has been restricted from accessing the platform.
+ * DELETED: A user that has been deleted by an Admin.
+ */
 enum UserStatus {
     PENDING, APPROVED, BANNED, DELETED
 }
