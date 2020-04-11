b865eb9030ef141be03346ce8b49b9c58e7ad896
Freda Chen <fredachen98@gmail.com>
2020-04-11 19:41:43 -0400

    Code review MediaMetaDataController.java

--- server/src/main/java/worlds/server/MediaMetaDataController.java
+++ server/src/main/java/worlds/server/MediaMetaDataController.java
@@ -28,6 +28,10 @@ class MediaMetaDataController{
         this.assembler = assembler;
     }
 
+    /**
+     * GET method
+     * @return all mediametadatas in the database
+     */
     @GetMapping(value = "/mediametadatas", produces = "application/json; charset=UTF-8")
     Resources<Resource<MediaMetaData>> all() {
         List<Resource<MediaMetaData>> mediaMetaDatas = repository.findAll().stream()
@@ -38,13 +42,27 @@ class MediaMetaDataController{
             linkTo(methodOn(MediaMetaDataController.class).all()).withSelfRel()); 
     }
 
+    /**
+     * GET method
+     * 
+     * @param id id of a mediametadata
+     * @return A specific mediametadata
+     * @throws MediaMetaDataNotFoundException if mediametadata id is not found
+     */
     @GetMapping(value = "/mediametadatas/{id}", produces = "application/json; charset=UTF-8")
     Resource<MediaMetaData> one(@PathVariable String id) {
         MediaMetaData mediaMetaData = repository.findById(id)
-        .orElseThrow(() -> new UserNotFoundException(id));
+        .orElseThrow(() -> new MediaMetaDataNotFoundException(id));
         return assembler.toResource(mediaMetaData);
     }
 
+    /**
+     * POST method
+     * 
+     * @param newMediaMetaData
+     * @return Creates a new mediaMetaData in the database
+     * @throws URISyntaxException if failed to create new mediametadata object
+     */
     @PostMapping("/mediametadatas")
     ResponseEntity<?> newMediaMetaData(@RequestBody MediaMetaData newMediaMetaData) throws URISyntaxException {
 
@@ -55,16 +73,24 @@ class MediaMetaDataController{
         return ResponseEntity.created(new URI(resource.getId().expand().getHref())).body(resource);
     }
 
+    /**
+     * PUT method
+     * 
+     * @param updateMediaMetaData a mediametadata object with desired attributes
+     * @param id  id of current mediametadata object
+     * @return An updated version of current mediametadata object with new attributes
+     * @throws MediaMetaDataNotFoundException 
+     */
     @PutMapping("/mediametadatas/{id}")
-    Resource<MediaMetaData> updateMediaMetadata(@Valid @RequestBody MediaMetaData newMediaMetaData,
+    Resource<MediaMetaData> updateMediaMetadata(@Valid @RequestBody MediaMetaData updateMediaMetaData,
     @PathVariable final String id) throws MediaMetaDataNotFoundException {
         MediaMetaData current = repository.findById(id)
         .orElseThrow(() -> new MediaMetaDataNotFoundException("media does not exist with id ::" + id));
 
-        current.setCreatorId(newMediaMetaData.getCreatorId());
-        current.setRoomInformation(newMediaMetaData.getRoomInformation());
-        current.setUrlToMedia(newMediaMetaData.getUrlToMedia());
-        current.setDateUploaded(newMediaMetaData.getDateUploaded());
+        current.setCreatorId(updateMediaMetaData.getCreatorId());
+        current.setRoomInformation(updateMediaMetaData.getRoomInformation());
+        current.setUrlToMedia(updateMediaMetaData.getUrlToMedia());
+        current.setDateUploaded(updateMediaMetaData.getDateUploaded());
         final MediaMetaData updatedMetaData = repository.save(current);
         return assembler.toResource(updatedMetaData);
     }
