a5f8fccf01370a0a3c86221c412ca53708cd0730
Freda Chen <fredachen98@gmail.com>
2020-04-11 19:59:41 -0400

    MediaMetadataNotFoundException.java review

--- server/src/main/java/worlds/server/MediaMetaDataNotFoundException.java
+++ server/src/main/java/worlds/server/MediaMetaDataNotFoundException.java
@@ -2,7 +2,11 @@ package worlds.server;
 
 @SuppressWarnings("serial")
 class MediaMetaDataNotFoundException extends RuntimeException {
-
+    
+    /**
+     * 
+     * @param id mediametadata id
+     */
     MediaMetaDataNotFoundException(String id) {
         super("Could not find media metadata with id: " + id);
     }
