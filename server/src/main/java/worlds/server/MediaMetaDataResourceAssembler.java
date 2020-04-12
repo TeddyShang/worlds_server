20bc35ec659ace9ef9de52f3226fd140fdbfc287
Freda Chen <fredachen98@gmail.com>
2020-04-11 20:11:24 -0400

    MediaMetaDataResourceAssembler.java reviewed

--- server/src/main/java/worlds/server/MediaMetaDataResourceAssembler.java
+++ server/src/main/java/worlds/server/MediaMetaDataResourceAssembler.java
@@ -8,6 +8,10 @@ import org.springframework.stereotype.Component;
 @Component
 class MediaMetaDataResourceAssembler implements ResourceAssembler<MediaMetaData, Resource<MediaMetaData>> {
 
+  /**
+   * @param mediaMetaData a specific mediaMetaData
+   * @return a mediaMetaData resource with a self link and a link to all mediaMetaDatas in DB
+   */
   @Override
   public Resource<MediaMetaData> toResource(MediaMetaData mediaMetaData) {
     return new Resource<>(mediaMetaData,
